6853b30a2343c8f9ebc63c571ffcf66b776cf5b1
Mudassir <mudassirriaz32@gmail.com>
2024-10-26 02:08:03 +0500

    Initial commit on CypressAutomation

---
+++ cypress/e2e/firsttest.cy.js
@@ -0,0 +1,7 @@
+
+describe('Launch Aplication', () => {
+    it('Launch The Test Application', () => {
+        cy.visit('https://parabank.parasoft.com/parabank/index.htm;jsessionid=5A300A4C97B648C95CB1955DC09E30B5')
+        
+    })
+  })
